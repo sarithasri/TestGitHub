9356e6392d36aacc8fd8a3e147910cc626ee3f03
rensa06 <rensa06@ca.com>
2016-01-07 17:05:04 +0530

    ff

--- src/main/java/com/ca/SampleTest.java
+++ src/main/java/com/ca/SampleTest.java
@@ -4,5 +4,6 @@ class SampleTest
 	public static void main(String args[]){
 		System.out.println("This is sample TEst messgae info");
 		System.out.println("This is sample TEst messgae info1");
+		System.out.println("This is sample TEst messgae info1");
 	}
 }
