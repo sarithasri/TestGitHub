429442fd3880f132fb28435d3c42d4cce2545f5e
rensa06 <rensa06@ca.com>
2016-01-07 16:54:10 +0530

    added

--- src/main/java/com/ca/SampleTest.java
+++ src/main/java/com/ca/SampleTest.java
@@ -3,5 +3,6 @@ class SampleTest
 {
 	public static void main(String args[]){
 		System.out.println("This is sample TEst messgae info");
+		System.out.println("This is sample TEst messgae info1");
 	}
 }
