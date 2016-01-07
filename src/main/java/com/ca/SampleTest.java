622789f37b18082e2aabee1184247198d29ec6b0
sarithasri <sarithasri09@gmail.com>
2016-01-07 17:06:31 +0530

    dfdsf

--- src/main/java/com/ca/SampleTest.java
+++ src/main/java/com/ca/SampleTest.java
@@ -4,7 +4,7 @@ class SampleTest
 	public static void main(String args[]){
 		System.out.println("This is sample TEst messgae info");
 		System.out.println("This is sample TEst messgae info1");
-		System.out.println("This is sample TEst messgae info1");
+		
 		
 	}
 }
